7f0d4c65252c411f33573d9fa3ac9cf33a808318
neChak <34412522+cjy8922@users.noreply.github.com>
2021-01-02 00:20:09 +0900

    JunYeong 20210101 삼성 1204

---
+++ JunYeong_20210101_SWEA_1204.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	cin>>T;
+
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+		int i, m, score, scores[101];
+		for(i=0; i<=100; i++) {
+			scores[i]=0;	
+		}
+		
+		scanf("%d", &m);
+		for(i=1; i<=1000; i++) {
+			scanf("%d", &score);
+			scores[score]++;
+		}
+		
+		int max=0, idx;
+		for(i=0; i<=100; i++) {
+			if(scores[i] >= max) {
+				max = scores[i];
+				idx = i;
+			}
+		}
+		
+		printf("#%d %d\n", test_case, idx);
+	}
+
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}

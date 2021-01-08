5588933bbc3f82a1f2402b9cb27301fd24434ed1
neChak <34412522+cjy8922@users.noreply.github.com>
2021-01-09 00:18:59 +0900

    JunYeong 20210108 삼성 3143 가장 빠른 문자열 타이핑

--- JunYeong_20210108_SWEA_3143.cpp
+++ JunYeong_20210108_SWEA_3143.cpp
@@ -10,21 +10,21 @@ int main(int argc, char** argv)
 	for(test_case = 1; test_case <= T; ++test_case)
 	{
 		string A, B;
-    cin >> A >> B;
-    int i=0, res=0;
-    int size_A = A.length();
-    int size_B = B.length();
-    
-    while(1) {
-    	int exist = A.find(B, i);
-    	if(exist != string::npos) {
-    		++res;
-    		i = exist + size_B - 1;
+    		cin >> A >> B;
+    		int i=0, res=0;
+    		int size_A = A.length();
+    		int size_B = B.length();
+	    
+    		while(1) {
+    			int exist = A.find(B, i);
+    			if(exist != string::npos) {
+    				++res;
+    				i = exist + size_B - 1;
 			}
 			else break;
 		}
 		res = size_A - res * size_B + res;
-        printf("#%d %d\n", test_case, res);
+        	printf("#%d %d\n", test_case, res);
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
